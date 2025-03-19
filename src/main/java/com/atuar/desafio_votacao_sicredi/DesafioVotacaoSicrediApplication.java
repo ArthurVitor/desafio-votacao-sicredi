a7a1166a569d4d71c61fa8da91aed6d7edca64f9
ArthurVitor <arthurvitorgn@gmail.com>
2025-03-19 15:32:03 -0300

    initial commit

---
+++ src/main/java/com/atuar/desafio_votacao_sicredi/DesafioVotacaoSicrediApplication.java
@@ -0,0 +1,13 @@
+package com.atuar.desafio_votacao_sicredi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioVotacaoSicrediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioVotacaoSicrediApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atuar/desafio_votacao_sicredi/DesafioVotacaoSicrediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atuar.desafio_votacao_sicredi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVotacaoSicrediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
