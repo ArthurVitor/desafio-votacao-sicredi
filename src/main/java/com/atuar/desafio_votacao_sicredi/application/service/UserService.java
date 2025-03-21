c370dd828ce0a8794e3d8958062ebb7d4a406d02
ArthurVitor <arthurvitorgn@gmail.com>
2025-03-21 16:41:19 -0300

    feat; changed static class to mapstruct

--- src/main/java/com/atuar/desafio_votacao_sicredi/application/service/UserService.java
+++ src/main/java/com/atuar/desafio_votacao_sicredi/application/service/UserService.java
@@ -4,7 +4,6 @@ import com.atuar.desafio_votacao_sicredi.application.dto.Page.PageDto;
 import com.atuar.desafio_votacao_sicredi.application.dto.User.CreateUserDto;
 import com.atuar.desafio_votacao_sicredi.application.dto.User.ListUserDto;
 import com.atuar.desafio_votacao_sicredi.application.exception.NotFound.NotFoundException;
-import com.atuar.desafio_votacao_sicredi.application.mapper.PageMapper;
 import com.atuar.desafio_votacao_sicredi.application.mapper.UserMapper;
 import com.atuar.desafio_votacao_sicredi.domain.entity.User;
 import com.atuar.desafio_votacao_sicredi.domain.repository.UserRepository;
@@ -15,25 +14,27 @@ import org.springframework.stereotype.Service;
 @Service()
 public class UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     public ListUserDto create(CreateUserDto dto) {
-        User user = UserMapper.toEntity(dto);
+        User user = userMapper.toEntity(dto);
 
-        return UserMapper.toListDto(this.userRepository.save(user));
+        return userMapper.toListDto(this.userRepository.save(user));
     }
 
     public PageDto<ListUserDto> getAll(Pageable pageable) {
-        Page<ListUserDto> users = this.userRepository.findAll(pageable).map(UserMapper::toListDto);
+        Page<ListUserDto> users = this.userRepository.findAll(pageable).map(userMapper::toListDto);
 
-        return PageMapper.toPageDto(users);
+        return userMapper.toPageDto(users);
     }
 
     public ListUserDto getById(Long id) {
-        return UserMapper.toListDto(this.userRepository.findById(id).orElseThrow(() -> new NotFoundException("Couldn't find user with id: " + id)));
+        return userMapper.toListDto(this.userRepository.findById(id).orElseThrow(() -> new NotFoundException("Couldn't find user with id: " + id)));
     }
 
     public void delete(Long id) {
